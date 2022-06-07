fc449b5a4dfd71e861144caca7455f10aea7fcc3
Vitor-Manoel02 <vitor@investidorsardinha.com.br>
2022-06-07 16:28:56 -0300

    cronometro pronto

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{type?: "button" | "submit" | "reset" | undefined}> { //props opcional = ?:
+class Botao extends React.Component<{type?: "button" | "submit" | "reset" | undefined, //props opcional = ?:
+  onClick?: ()=> void }> { 
   render() {
-    const {type="button"} = this.props;
+    const {type="button", onClick} = this.props;
     return (
-      <button  className={style.botao}>
+      <button onClick={onClick}  className={style.botao}>
         {this.props.children}
       </button>
     )
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -12,6 +12,15 @@ interface Props {
 export default function Cronometro({selecionado }: Props) {
   const [tempo, setTempo] = useState<number>();
 
+  function regressiva(contador: number = 0){
+    setTimeout(() => {
+      if(contador > 0){ 
+        setTempo(contador - 1);
+        return regressiva(contador - 1)
+      }
+    },1000)
+  }
+
   useEffect(() => {
     if(selecionado?.tempo){
       setTempo(tempoEmSegundos(selecionado?.tempo))
@@ -26,7 +35,7 @@ export default function Cronometro({selecionado }: Props) {
       <div className={style.relogioWrapper}>
         <Relogio tempo={tempo} />
       </div>
-      <Botao>
+      <Botao onClick={()=> regressiva(tempo)}>
         Começar!
       </Botao>
     </div>
